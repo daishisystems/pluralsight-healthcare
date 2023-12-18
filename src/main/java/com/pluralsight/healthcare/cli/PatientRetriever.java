fafe288a09fc905a4a3c50a4baf63305602a9bf4
Paul Mooney <paul@daishisystems.com>
2023-12-18 16:32:33 +0000

    Added simple CLI method stubs and project build configuration

--- src/main/java/com/pluralsight/healthcare/cli/PatientRetriever.java
+++ src/main/java/com/pluralsight/healthcare/cli/PatientRetriever.java
@@ -4,5 +4,21 @@ public class PatientRetriever {
 
     public static void main(String... args) {
         System.out.println("PatientRetriever started!");
+        if (args.length == 0) {
+            System.out.println("Please provide a facility name as first argument.");
+            return;
+        }
+
+        try {
+            retrievePatients(args[0]);
+        } catch (Exception e) {
+            System.out.println("Unexpected error");
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void retrievePatients(String facilityId) {
+        System.out.println("Retrieving patients for facility " + facilityId);
     }
 }
