b276f13c0842db35b66bcb4b1a1210592bb7a191
Paul Mooney <paul@daishisystems.com>
2023-12-18 16:01:11 +0000

    Initial commit

---
+++ src/main/java/com/pluralsight/healthcare/cli/PatientRetriever.java
@@ -0,0 +1,8 @@
+package com.pluralsight.healthcare.cli;
+
+public class PatientRetriever {
+
+    public static void main(String... args) {
+        System.out.println("PatientRetriever started!");
+    }
+}
