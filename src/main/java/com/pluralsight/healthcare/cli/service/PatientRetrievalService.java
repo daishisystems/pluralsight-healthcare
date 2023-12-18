c11b724df7f3fe7d1dd4fcedf6f1974cf4b7220a
Paul Mooney <paul@daishisystems.com>
2023-12-18 22:02:41 +0000

    Improved HTTP Client usage

--- src/main/java/com/pluralsight/healthcare/cli/service/PatientRetrievalService.java
+++ src/main/java/com/pluralsight/healthcare/cli/service/PatientRetrievalService.java
@@ -8,7 +8,10 @@ import java.net.http.HttpResponse;
 
 public class PatientRetrievalService {
     private static final String PATIENTS_URI = "https://randomuser.me/api/?results=6";
-    private static final HttpClient HTTP_CLIENT = HttpClient.newHttpClient();
+    private static final HttpClient HTTP_CLIENT = HttpClient
+            .newBuilder()
+            .followRedirects(HttpClient.Redirect.ALWAYS)
+            .build();
 
     public String getPatientsFor(String facilityId) {
         HttpRequest request = HttpRequest
@@ -17,7 +20,12 @@ public class PatientRetrievalService {
                 .build();
         try {
             HttpResponse<String> httpResponse = HTTP_CLIENT.send(request, HttpResponse.BodyHandlers.ofString());
-            return httpResponse.body();
+            return switch (httpResponse.statusCode()) {
+                case 200 -> httpResponse.body();
+                case 404 -> "";
+                default ->
+                        throw new RuntimeException("Patients API call failed with status code " + httpResponse.statusCode());
+            };
         } catch (IOException | InterruptedException e) {
             throw new RuntimeException("Could not call patients API", e);
         }
