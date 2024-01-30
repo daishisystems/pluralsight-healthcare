45cc93e68b8c48a580305178ad9370d583881162
Paul Mooney <paul@daishisystems.com>
2024-01-30 21:49:47 +0000

    Added patient API exception-handling and JSON parsing

--- pluralsight-healthcare-server/src/main/java/com/pluralsight/healthcare/server/PatientResource.java
+++ pluralsight-healthcare-server/src/main/java/com/pluralsight/healthcare/server/PatientResource.java
@@ -2,7 +2,9 @@ package com.pluralsight.healthcare.server;
 
 import com.pluralsight.healthcare.domain.Patient;
 import com.pluralsight.healthcare.repository.PatientRepository;
+import com.pluralsight.healthcare.repository.RepositoryException;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.NotFoundException;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
@@ -25,10 +27,15 @@ public class PatientResource {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public List<Patient> getPatients() {
-        return patientRepository
-                .getAllPatients()
-                .stream()
-                .sorted(Comparator.comparing(Patient::id))
-                .toList();
+        try {
+            return patientRepository
+                    .getAllPatients()
+                    .stream()
+                    .sorted(Comparator.comparing(Patient::id))
+                    .toList();
+        } catch (RepositoryException e) {
+            LOG.error("Could not retrieve patients from teh database.", e);
+            throw new NotFoundException();
+        }
     }
 }
